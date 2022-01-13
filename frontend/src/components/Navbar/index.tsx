4e1e918169dee3fdba54c5fa5f41c677abb075d6
Hugo3120 <hugoleo3120@gmail.com>
2022-01-13 10:16:07 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,16 +2,16 @@ import { ReactComponent as GithubIcon } from 'assets/img/GitHub.svg';
 import './styles.css';
 
 function Navbar() {
-   
+
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
                     <a href="https://github.com/Hugo3120">
-                        <div>
+                        <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p>/devsuperior</p>
+                            <p className="dsmovie-contact-link">/hugoleonardo</p>
                         </div>
 
                     </a>
