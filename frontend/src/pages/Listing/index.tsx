414efe6948f365301a609b11b0d31591946bd30c
Hugo3120 <hugoleo3120@gmail.com>
2022-01-14 07:00:27 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -8,21 +8,22 @@ function Listing() {
 
             <div className="container">
                 <div className="row">
-                    <div className="col">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
-                    <div className="col">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
                         <MovieCard />
                     </div>
+
                 </div>
 
             </div>
